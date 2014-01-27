ad88bcb4fb6629722a913649d4a57e24a856575d
Dan Treasure <dtreasure@getjive.com>
2014-01-26 17:54:41 -0700

    Removed the code that students need to complete

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 $('document').ready(function(){
-	$("#total-calculate").click(function(){
-		var subtotal = $('#sub-total').val();
-		var total = subtotal * 1.2;
-		$('#total-amount').html("$" + total);
-		$('#sub-total').val('');
-	});
+	//Code goes here
 });
 var waitlist = [];
 var waiting = {};
 
-window.new_party_joined = function(person) {
-	waitlist.unshift(person);
+window.new_party_joined = function(person) {	
 	if(person in waiting){
 		return;
 	} else{
 		waiting[person] = person;
 		$('.list-group').append("<li class='list-group-item' id ='" + person + "''>" + person + "</li>");
 	}
+	//Code goes below here
 }
 
 window.assign_to_table = function() {
-	var name = waitlist.pop();
-	console.log("assign to table called", name);
-	$('#' + name).remove()
+	//Code goes here
 }
